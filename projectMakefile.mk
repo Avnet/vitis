67316068200c085a817669e9c6f5aebee10bf01e
zedman2000 <daniel.rozwood@avnet.com>
2020-10-20 09:47:54 -1000

    Adjusted for proper BASH use

--- projectMakefile.mk
+++ projectMakefile.mk
@@ -103,7 +103,7 @@ ifneq (,$(wildcard ${HDL_PROJECTS_FOLDER}/${HDL_PROJECT_NAME}/${HDL_BOARD_NAME}_
 	@echo '        ' ${HDL_PROJECTS_FOLDER}/${HDL_PROJECT_NAME}/${HDL_BOARD_NAME}_${PLNX_VER}/${HDL_BOARD_NAME}.xsa
 else
 	@echo -e '${CSTR} Making XSA'
-	@echo 'xsa: \n	@vivado -mode batch -notrace -source make_${HDL_PROJECT_NAME}.tcl \
+	@echo -e 'xsa: \n	@vivado -mode batch -notrace -source make_${HDL_PROJECT_NAME}.tcl \
 	                            -tclargs ${HDL_BOARD_NAME} ${HDL_PROJECT_NAME}' > ${HDL_SCRIPTS_FOLDER}/${MAKENAME}
 	$(MAKE) -f ${MAKENAME} -C ${HDL_SCRIPTS_FOLDER} xsa
 	#@echo
@@ -131,7 +131,7 @@ ifneq (,$(wildcard ${PETALINUX_PROJECTS_FOLDER}/${PETALINUX_PROJECT_NAME}.bsp))
 	@echo '        ' ${PETALINUX_PROJECTS_FOLDER}/${PETALINUX_PROJECT_NAME}.bsp
 else
 	@echo -e '${CSTR} Making PLNX Project'
-	@echo 'SHELL := /bin/bash \n\nplnx: \n	./make_${PETALINUX_ROOTFS_NAME}.sh' > ${PETALINUX_SCRIPTS_FOLDER}/${MAKENAME}
+	@echo -e 'plnx: \n	./make_${PETALINUX_ROOTFS_NAME}.sh' > ${PETALINUX_SCRIPTS_FOLDER}/${MAKENAME}
 	$(MAKE) -f ${MAKENAME} -C ${PETALINUX_SCRIPTS_FOLDER} plnx  
 	#@echo
 	#@echo
