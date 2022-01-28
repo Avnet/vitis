57e99cc3c6529d5191bb4d89a4be3e7424c1f01f
AlbertaBeef <mario.bergeron@avnet.com>
2022-01-28 13:05:24 -0500

    app: vadd: Update vector_add example.  Add profiling results.

--- app/vadd/src/krnl_vadd.cpp
+++ app/vadd/src/krnl_vadd.cpp
@@ -1,98 +1,120 @@
+/**
+* Copyright (C) 2019-2021 Xilinx, Inc
+*
+* Licensed under the Apache License, Version 2.0 (the "License"). You may
+* not use this file except in compliance with the License. A copy of the
+* License is located at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
+
 /*******************************************************************************
-Vendor: Xilinx
-Associated Filename: krnl_vadd.cpp
-Purpose: Vitis vector addition example
-*******************************************************************************
-Copyright (C) 2019 XILINX, Inc.
+Description:
 
-This file contains confidential and proprietary information of Xilinx, Inc. and
-is protected under U.S. and international copyright and other intellectual
-property laws.
+    This example uses the load/compute/store coding style which is generally
+    the most efficient for implementing kernels using HLS. The load and store
+    functions are responsible for moving data in and out of the kernel as
+    efficiently as possible. The core functionality is decomposed across one
+    of more compute functions. Whenever possible, the compute function should
+    pass data through HLS streams and should contain a single set of nested loops.
 
-DISCLAIMER
-This disclaimer is not a license and does not grant any rights to the materials
-distributed herewith. Except as otherwise provided in a valid license issued to
-you by Xilinx, and to the maximum extent permitted by applicable law:
-(1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND WITH ALL FAULTS, AND XILINX
-HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY,
-INCLUDING BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT, OR
-FITNESS FOR ANY PARTICULAR PURPOSE; and (2) Xilinx shall not be liable (whether
-in contract or tort, including negligence, or under any other theory of
-liability) for any loss or damage of any kind or nature related to, arising under
-or in connection with these materials, including for any direct, or any indirect,
-special, incidental, or consequential loss or damage (including loss of data,
-profits, goodwill, or any type of loss or damage suffered as a result of any
-action brought by a third party) even if such damage or loss was reasonably
-foreseeable or Xilinx had been advised of the possibility of the same.
+    HLS stream objects are used to pass data between producer and consumer
+    functions. Stream read and write operations have a blocking behavior which
+    allows consumers and producers to synchronize with each other automatically.
 
-CRITICAL APPLICATIONS
-Xilinx products are not designed or intended to be fail-safe, or for use in any
-application requiring fail-safe performance, such as life-support or safety
-devices or systems, Class III medical devices, nuclear facilities, applications
-related to the deployment of airbags, or any other applications that could lead
-to death, personal injury, or severe property or environmental damage
-(individually and collectively, "Critical Applications"). Customer assumes the
-sole risk and liability of any use of Xilinx products in Critical Applications,
-subject only to applicable laws and regulations governing limitations on product
-liability.
-
-THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
-ALL TIMES.
+    The dataflow pragma instructs the compiler to enable task-level pipelining.
+    This is required for to load/compute/store functions to execute in a parallel
+    and pipelined manner. Here the kernel loads, computes and stores NUM_WORDS integer values per
+    clock cycle and is implemented as below:
+                                       _____________
+                                      |             |<----- Input Vector 1 from Global Memory
+                                      |  load_input |       __
+                                      |_____________|----->|  |
+                                       _____________       |  | in1_stream
+Input Vector 2 from Global Memory --->|             |      |__|
+                               __     |  load_input |        |
+                              |  |<---|_____________|        |
+                   in2_stream |  |     _____________         |
+                              |__|--->|             |<--------
+                                      | compute_add |      __
+                                      |_____________|---->|  |
+                                       ______________     |  | out_stream
+                                      |              |<---|__|
+                                      | store_result |
+                                      |______________|-----> Output result to Global Memory
 
 *******************************************************************************/
 
-//------------------------------------------------------------------------------
-//
-// kernel:  vadd
-//
-// Purpose: Demonstrate Vector Add Kernel
-//
+// Includes
+#include <stdint.h>
+#include <hls_stream.h>
 
-#define BUFFER_SIZE 256
-#define DATA_SIZE 4096 
-//TRIPCOUNT identifier
-const unsigned int c_len = DATA_SIZE / BUFFER_SIZE;
-const unsigned int c_size = BUFFER_SIZE;
+#define DATA_SIZE 4096
 
-/*
-    Vector Addition Kernel Implementation 
-    Arguments:
-        in1   (input)     --> Input Vector1
-        in2   (input)     --> Input Vector2
-        out_r   (output)    --> Output Vector
-        size  (input)     --> Size of Vector in Integer
-*/
+// TRIPCOUNT identifier
+const int c_size = DATA_SIZE;
+
+static void load_input(uint32_t* in, hls::stream<uint32_t>& inStream, int size) {
+mem_rd:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        inStream << in[i];
+    }
+}
+
+static void compute_add(hls::stream<uint32_t>& in1_stream,
+                        hls::stream<uint32_t>& in2_stream,
+                        hls::stream<uint32_t>& out_stream,
+                        int size) {
+// The kernel is operating with vector of NUM_WORDS integers. The + operator performs
+// an element-wise add, resulting in NUM_WORDS parallel additions.
+execute:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        out_stream << (in1_stream.read() + in2_stream.read());
+    }
+}
+
+static void store_result(uint32_t* out, hls::stream<uint32_t>& out_stream, int size) {
+mem_wr:
+    for (int i = 0; i < size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+        out[i] = out_stream.read();
+    }
+}
 
 extern "C" {
-void krnl_vadd(const unsigned int *in1, // Read-Only Vector 1
-          const unsigned int *in2, // Read-Only Vector 2
-          unsigned int *out_r,     // Output Result
-          int size                 // Size in integer
-) {
 
-    unsigned int v1_buffer[BUFFER_SIZE];   // Local memory to store vector1
+/*
+    Vector Addition Kernel
 
-    //Per iteration of this loop perform BUFFER_SIZE vector addition
-    for (int i = 0; i < size; i += BUFFER_SIZE) {
-       #pragma HLS LOOP_TRIPCOUNT min=c_len max=c_len
-        int chunk_size = BUFFER_SIZE;
-        //boundary checks
-        if ((i + BUFFER_SIZE) > size)
-            chunk_size = size - i;
+    Arguments:
+        in1  (input)  --> Input vector 1
+        in2  (input)  --> Input vector 2
+        out  (output) --> Output vector
+        size (input)  --> Number of elements in vector
+*/
 
-        read1: for (int j = 0; j < chunk_size; j++) {
-           #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-            v1_buffer[j] = in1[i + j];
-        }
+void krnl_vadd(uint32_t* in1, uint32_t* in2, uint32_t* out, int size) {
+#pragma HLS INTERFACE m_axi port = in1 bundle = gmem0
+#pragma HLS INTERFACE m_axi port = in2 bundle = gmem1
+#pragma HLS INTERFACE m_axi port = out bundle = gmem0
 
-        //Burst reading B and calculating C and Burst writing 
-        // to  Global memory
-        vadd_writeC: for (int j = 0; j < chunk_size; j++) {
-           #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
-            //perform vector addition
-            out_r[i+j] = v1_buffer[j] + in2[i+j];
-        }
+    static hls::stream<uint32_t> in1_stream("input_stream_1");
+    static hls::stream<uint32_t> in2_stream("input_stream_2");
+    static hls::stream<uint32_t> out_stream("output_stream");
 
-    }
+#pragma HLS dataflow
+    // dataflow pragma instruct compiler to run following three APIs in parallel
+    load_input(in1, in1_stream, size);
+    load_input(in2, in2_stream, size);
+    compute_add(in1_stream, in2_stream, out_stream, size);
+    store_result(out, out_stream, size);
 }
 }
--- app/vadd/src/vadd.cpp
+++ app/vadd/src/vadd.cpp
@@ -41,6 +41,13 @@ THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS PART OF THIS FILE AT
 ALL TIMES.
 
 *******************************************************************************/
+#define OCL_CHECK(error, call)                                                                   \
+    call;                                                                                        \
+    if (error != CL_SUCCESS) {                                                                   \
+        printf("%s:%d Error calling " #call ", error code is: %d\n", __FILE__, __LINE__, error); \
+        exit(EXIT_FAILURE);                                                                      \
+    }
+
 #include <stdlib.h>
 #include <fstream>
 #include <iostream>
@@ -52,6 +59,8 @@ static const std::string error_message =
     "Error: Result mismatch:\n"
     "i = %d CPU result = %d Device result = %d\n";
 
+#define _PROFILE_ 1
+
 int main(int argc, char* argv[]) {
 
     //TARGET_DEVICE macro needs to be passed from gcc command line
@@ -60,7 +69,7 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-    char* xclbinFilename = argv[1];
+    std::string xclbinFilename = argv[1];
     
     // Compute the size of array in bytes
     size_t size_in_bytes = DATA_SIZE * sizeof(int);
@@ -70,6 +79,11 @@ int main(int argc, char* argv[]) {
     
     std::vector<cl::Device> devices;
     cl::Device device;
+    cl_int err;
+    cl::Context context;
+    cl::CommandQueue q;
+    cl::Kernel krnl_vector_add;
+    cl::Program program;
     std::vector<cl::Platform> platforms;
     bool found_device = false;
 
@@ -96,9 +110,15 @@ int main(int argc, char* argv[]) {
     }
 
     // Creating Context and Command Queue for selected device
-    cl::Context context(device);
-    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE);
+    OCL_CHECK(err, context = cl::Context(device, NULL, NULL, NULL, &err));
+    OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
 
+    std::cout << "INFO: Reading " << xclbinFilename << std::endl;
+    FILE* fp;
+    if ((fp = fopen(xclbinFilename.c_str(), "r")) == nullptr) {
+        printf("ERROR: %s xclbin not available please build\n", xclbinFilename.c_str());
+        exit(EXIT_FAILURE);
+    }
     // Load xclbin 
     std::cout << "Loading: '" << xclbinFilename << "'\n";
     std::ifstream bin_file(xclbinFilename, std::ifstream::binary);
@@ -112,48 +132,63 @@ int main(int argc, char* argv[]) {
     cl::Program::Binaries bins;
     bins.push_back({buf,nb});
     devices.resize(1);
-    cl::Program program(context, devices, bins);
+    OCL_CHECK(err, program = cl::Program(context, devices, bins, NULL, &err));
     
     // This call will get the kernel object from program. A kernel is an 
     // OpenCL function that is executed on the FPGA. 
-    cl::Kernel krnl_vector_add(program,"krnl_vadd");
-    
+    OCL_CHECK(err, krnl_vector_add = cl::Kernel(program,"krnl_vadd", &err));
+
     // These commands will allocate memory on the Device. The cl::Buffer objects can
     // be used to reference the memory locations on the device. 
-    cl::Buffer buffer_a(context, CL_MEM_READ_ONLY, size_in_bytes);
-    cl::Buffer buffer_b(context, CL_MEM_READ_ONLY, size_in_bytes);
-    cl::Buffer buffer_result(context, CL_MEM_WRITE_ONLY, size_in_bytes);
+    OCL_CHECK(err, cl::Buffer buffer_a(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
+    OCL_CHECK(err, cl::Buffer buffer_b(context, CL_MEM_READ_ONLY, size_in_bytes, NULL, &err));
+    OCL_CHECK(err, cl::Buffer buffer_result(context, CL_MEM_WRITE_ONLY, size_in_bytes, NULL, &err));
     
     //set the kernel Arguments
     int narg=0;
-    krnl_vector_add.setArg(narg++,buffer_a);
-    krnl_vector_add.setArg(narg++,buffer_b);
-    krnl_vector_add.setArg(narg++,buffer_result);
-    krnl_vector_add.setArg(narg++,DATA_SIZE);
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++,buffer_a));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++,buffer_b));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++,buffer_result));
+    OCL_CHECK(err, err = krnl_vector_add.setArg(narg++,DATA_SIZE));
 
     //We then need to map our OpenCL buffers to get the pointers
-    int *ptr_a = (int *) q.enqueueMapBuffer (buffer_a , CL_TRUE , CL_MAP_WRITE , 0, size_in_bytes);
-    int *ptr_b = (int *) q.enqueueMapBuffer (buffer_b , CL_TRUE , CL_MAP_WRITE , 0, size_in_bytes);
-    int *ptr_result = (int *) q.enqueueMapBuffer (buffer_result , CL_TRUE , CL_MAP_READ , 0, size_in_bytes);
-
-    //setting input data
-    for(int i = 0 ; i< DATA_SIZE; i++){
-	    ptr_a[i] = 10;
-	    ptr_b[i] = 20;
-    }
+    int *ptr_a;
+    int *ptr_b;
+    int *ptr_result;
+    OCL_CHECK(err, ptr_a = (int*)q.enqueueMapBuffer (buffer_a , CL_TRUE , CL_MAP_WRITE , 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err, ptr_b = (int*)q.enqueueMapBuffer (buffer_b , CL_TRUE , CL_MAP_WRITE , 0, size_in_bytes, NULL, NULL, &err));
+    OCL_CHECK(err, ptr_result = (int*)q.enqueueMapBuffer (buffer_result , CL_TRUE , CL_MAP_READ , 0, size_in_bytes, NULL, NULL, &err));
 
     // Data will be migrated to kernel space
-    q.enqueueMigrateMemObjects({buffer_a,buffer_b},0/* 0 means from host*/);
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_a,buffer_b},0/* 0 means from host*/));
 
     //Launch the Kernel
-    q.enqueueTask(krnl_vector_add);
+    //OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add));
+
+    // Profiling Objects
+    cl_ulong start = 0;
+    cl_ulong end = 0;
+    double diff_prof = 0.0f;
+    cl::Event event_sp;
+
+    // Launch the kernel
+    OCL_CHECK(err, err = q.enqueueTask(krnl_vector_add, NULL, &event_sp));
+
+    // profiling
+    clWaitForEvents(1, (const cl_event*)&event_sp);
+#if _PROFILE_
+    event_sp.getProfilingInfo(CL_PROFILING_COMMAND_START, &start);
+    event_sp.getProfilingInfo(CL_PROFILING_COMMAND_END, &end);
+    diff_prof = end - start;
+    std::cout << (diff_prof / 1000000) << "ms" << std::endl;
+#endif
 
     // The result of the previous kernel execution will need to be retrieved in
     // order to view the results. This call will transfer the data from FPGA to
     // source_results vector
-    q.enqueueMigrateMemObjects({buffer_result},CL_MIGRATE_MEM_OBJECT_HOST);
+    OCL_CHECK(err, q.enqueueMigrateMemObjects({buffer_result},CL_MIGRATE_MEM_OBJECT_HOST));
 
-    q.finish();
+    OCL_CHECK(err, q.finish());
 
     //Verify the result
     int match = 0;
@@ -166,10 +201,10 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    q.enqueueUnmapMemObject(buffer_a , ptr_a);
-    q.enqueueUnmapMemObject(buffer_b , ptr_b);
-    q.enqueueUnmapMemObject(buffer_result , ptr_result);
-    q.finish();
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_a , ptr_a));
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_b , ptr_b));
+    OCL_CHECK(err, err = q.enqueueUnmapMemObject(buffer_result , ptr_result));
+    OCL_CHECK(err, err = q.finish());
 
     std::cout << "TEST " << (match ? "FAILED" : "PASSED") << std::endl; 
     return (match ? EXIT_FAILURE :  EXIT_SUCCESS);
